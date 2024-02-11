5177976c9475cd9720ff99a0fb9af33b111a50a7
AJAIN47 <AJAIN47@UNCC.EDU>
2024-02-11 17:50:27 -0500

    Angular code changes

--- server/server.js
+++ server/server.js
@@ -2,17 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+
 const budget = require("./mock-data.json");
 
-app.use('/', express.static('public'));
+console.log(budget);
+
 app.use(cors());
 
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-})
-
 app.get('/budget', (req, res) => {
+    console.log(budget);
     res.json(budget);
 });
 
